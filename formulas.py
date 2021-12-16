2137b661c540f3c83ecb801302656b85097f99b7
Akshat <akshatm408@gmail.com>
2021-12-16 10:48:00 +0530

    Created Formulas

---
+++ formulas.py
@@ -0,0 +1,10 @@
+
+
+
+
+def percentage_change(initial, final):
+    return ((final-initial)/initial)*100
+
+def percentage_leveraged_change(initial, final, leverage):
+    return ((final-initial)/initial)*100*leverage
+
---
+++ momentum_bot_v4.py
@@ -0,0 +1,5 @@
+from formulas import *
+
+
+
+print(percentage_leveraged_change(1,1.1,10))
